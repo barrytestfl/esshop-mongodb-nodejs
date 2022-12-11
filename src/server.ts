71ed8da331062e8234eb846696ffc3ca45438d4f
salman1992 <samadi.salman1992@gmail.com>
2022-12-12 02:41:17 +0330

    add conteroller to server .ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import 'dotenv/config';
 import App from './app';
-import PostsController from './posts/posts.controller';
 import AuthenticationController from './authentication/authentication.controller';
+import PostsController from './posts/posts.controller';
+import attributesController from 'attributes/attribute.controller';
+import attributeValuesModel from 'attributes/attributeValues.model';
+import attributeDetailessController from 'attributes/attributeDetails.controller';
+import BrandsController from 'brands/brand.controller';
+import groupsController from 'groups/group.controller';
+import productsController from 'products/product.controller';
+import productImagesController from 'ProductImages/productImage.controller';
+
 import validateEnv from './utils/validateEnv';
+import attributeValuessController from './attributes/attributeValues.controller';
 
 validateEnv();
 
@@ -10,6 +19,14 @@ const app = new App(
     [
         new AuthenticationController(),
         new PostsController(),
+
+        new attributesController(),
+        new attributeValuessController(),
+        new attributeDetailessController(),
+        new BrandsController(),
+        new groupsController(),
+        new productImagesController(),
+        new productsController(),
     ],
 );
 
